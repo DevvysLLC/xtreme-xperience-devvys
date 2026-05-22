c7800c6b49a28f93c5469b08234af009a798890b
Hanzala Kalota <196707655+HanzalaKal@users.noreply.github.com>
2026-05-22 19:20:43 +0500

    changes

--- src/app/api/v1/booking/config/route.ts
+++ src/app/api/v1/booking/config/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { initLogger } from '../../../../../core/logger/index'
-import { getMiddlewareClient } from '../../../../../server/middleware/index'
+import { BookingConfigService } from '../../../../../server/middleware/services/booking-config-service/index'
 
 const logger = initLogger().child({ name: 'booking-config-api' })
 
@@ -8,8 +8,7 @@ export const runtime = 'nodejs'
 
 export const GET = async (): Promise<NextResponse> => {
   try {
-    const client = await getMiddlewareClient()
-    const bookingConfigService = await client.getBookingConfigService()
+    const bookingConfigService = new BookingConfigService()
     const response = await bookingConfigService.getBookingConfig()
 
     return NextResponse.json(
